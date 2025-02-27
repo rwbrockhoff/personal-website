23602cd2c2213e3a290ff64e5f69d9840242eec6
rwbrockhoff <brockhoffrw@gmail.com>
2025-02-27 12:21:21 -0700

    style: add global style sheet and initial portfolio design migration

---
+++ src/data/projects.js
@@ -0,0 +1,29 @@
+// Project data
+export const projects = [
+  {
+    title: 'Tidytrek',
+    description: 'A modern e-commerce platform for outdoor gear with integrated mapping features.',
+    imageSrc: '/images/projects/duly_screen.gif',
+    tags: ['React', 'Node.js', 'PostgreSQL'],
+    demoLink: 'https://tidytrek.co',
+    githubLink: 'https://github.com/rwbrockhoff/tidytrek-frontend',
+  },
+  {
+    title: 'Looking Fox',
+    description:
+      'CMS web application built in React for professional photographers. Focuses on reusable components, using many tables and joins in Postgres, and incorporating Material UI and Bootstrap for a clean design.',
+    imageSrc: '/images/projects/lookingFoxScreen.gif',
+    tags: ['React', 'Node.js', 'PostgreSQL'],
+    demoLink: '',
+    githubLink: 'https://rwbrockhoff.github.io/ecrm/',
+  },
+  {
+    title: 'Oranges',
+    description:
+      'A full-stack, responsive game based on Apples to Apples. Built mobile-first in React, using socket.io as the bulk of the technology to communicate game information.',
+    imageSrc: '/images/projects/orangesdemo.gif',
+    tags: ['React', 'Firebase', 'Chart.js'],
+    demoLink: 'https://oranges2oranges2.firebaseapp.com/',
+    githubLink: 'https://github.com/rwbrockhoff/oranges',
+  },
+];
---
+++ src/data/skills.js
@@ -0,0 +1,16 @@
+// Skills data
+import reactIcon from '../assets/images/skills/react.png';
+import jsIcon from '../assets/images/skills/js.jpg';
+import nodeIcon from '../assets/images/skills/node.svg';
+import sqlIcon from '../assets/images/skills/postgres.png';
+import tsIcon from '../assets/images/skills/typescript.png';
+import jestIcon from '../assets/images/skills/jest.png';
+
+export const skills = [
+  { name: 'React', iconSrc: reactIcon, altText: 'React Icon' },
+  { name: 'JavaScript', iconSrc: jsIcon, altText: 'Javascript Icon' },
+  { name: 'Node.js', iconSrc: nodeIcon, altText: 'Node Icon' },
+  { name: 'PostgreSQL', iconSrc: sqlIcon, altText: 'Postgresql Icon' },
+  { name: 'Typescript', iconSrc: tsIcon, altText: 'Typescript Icon' },
+  { name: 'Jest', iconSrc: jestIcon, altText: 'Jest Icon' },
+];
