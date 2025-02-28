fe9ddbc0814cbe0f0641594ee4a5f561903ada36
rwbrockhoff <brockhoffrw@gmail.com>
2025-02-27 17:58:19 -0700

    style: add svg graphics for skill items

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,16 +1,18 @@
 // Skills data
-import reactIcon from '../assets/images/skills/react.png';
-import jsIcon from '../assets/images/skills/js.jpg';
+import reactIcon from '../assets/images/skills/react.svg';
+import jsIcon from '../assets/images/skills/javascript.svg';
 import nodeIcon from '../assets/images/skills/node.svg';
-import sqlIcon from '../assets/images/skills/postgres.png';
-import tsIcon from '../assets/images/skills/typescript.png';
-import jestIcon from '../assets/images/skills/jest.png';
+import sqlIcon from '../assets/images/skills/postgresql.svg';
+import tsIcon from '../assets/images/skills/typescript.svg';
+import jestIcon from '../assets/images/skills/jest.svg';
+import gitIcon from '../assets/images/skills/git.svg';
 
 export const skills = [
   { name: 'React', iconSrc: reactIcon, altText: 'React Icon' },
   { name: 'JavaScript', iconSrc: jsIcon, altText: 'Javascript Icon' },
   { name: 'Node.js', iconSrc: nodeIcon, altText: 'Node Icon' },
-  { name: 'PostgreSQL', iconSrc: sqlIcon, altText: 'Postgresql Icon' },
+  { name: 'SQL', iconSrc: sqlIcon, altText: 'Postgresql Icon' },
   { name: 'Typescript', iconSrc: tsIcon, altText: 'Typescript Icon' },
   { name: 'Jest', iconSrc: jestIcon, altText: 'Jest Icon' },
+  { name: 'Git', iconSrc: gitIcon, altText: 'Git Icon' },
 ];
