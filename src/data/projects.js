340fecfe690da190afd8b364bcd10362af708247
rwbrockhoff <brockhoffrw@gmail.com>
2025-02-27 21:51:50 -0700

    feat: project highlights, display project card image

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,30 +1,54 @@
+import laptopImage from '../assets/images/devices/mac.png';
+import mobileImage from '../assets/images/devices/iphone.png';
+
 // Project data
 export const projects = [
   {
     title: 'Tidytrek',
     description:
       'CMS web application built in React for professional photographers. Focuses on reusable components, using many tables and joins in Postgres, and incorporating Material UI and Bootstrap for a clean design.',
+    highlights: [
+      'Focuses on reusable components',
+      'Uses complex table relations in Postgres',
+      'Incorporates Material UI and Bootstrap',
+    ],
     imageSrc: '/images/projects/duly_screen.gif',
     tags: ['React', 'Node.js', 'PostgreSQL'],
     demoLink: 'https://tidytrek.co',
     githubLink: 'https://github.com/rwbrockhoff/tidytrek-frontend',
+    platformSrc: laptopImage,
+    platform: 'web',
   },
   {
     title: 'Looking Fox',
     description:
       'CMS web application built in React for professional photographers. Focuses on reusable components, using many tables and joins in Postgres, and incorporating Material UI and Bootstrap for a clean design.',
+    highlights: [
+      'Focuses on reusable components',
+      'Uses complex table relations in Postgres',
+      'Incorporates Material UI and Bootstrap',
+    ],
     imageSrc: '/images/projects/lookingFoxScreen.gif',
     tags: ['React', 'Node.js', 'PostgreSQL'],
     demoLink: '',
     githubLink: 'https://rwbrockhoff.github.io/ecrm/',
+    platformSrc: laptopImage,
+    platform: 'web',
   },
   {
     title: 'Oranges',
     description:
       'A full-stack, responsive game based on Apples to Apples. Built mobile-first in React, using socket.io as the bulk of the technology to communicate game information.',
+    highlights: [
+      'Focuses on reusable components',
+      'Uses complex table relations in Postgres',
+      'Incorporates Material UI and Bootstrap',
+    ],
     imageSrc: '/images/projects/orangesdemo.gif',
     tags: ['React', 'Firebase', 'Chart.js'],
     demoLink: 'https://oranges2oranges2.firebaseapp.com/',
     githubLink: 'https://github.com/rwbrockhoff/oranges',
+    platformSrc: mobileImage,
+    platform: 'mobile',
   },
 ];
