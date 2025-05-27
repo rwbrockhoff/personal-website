3fcb335eaf00f7596f895031943e06adf2877f09
rwbrockhoff <brockhoffrw@gmail.com>
2025-05-27 16:01:36 -0600

    minor: update packages and astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    svg: true,
+  vite: {
+    resolve: {
+      // alias path configuration
+      alias: {
+        '@components': path.resolve(__dirname, './src/components'),
+        '@data': path.resolve(__dirname, './src/data'),
+        '@images': path.resolve(__dirname, './src/assets/images'),
+        '@layouts': path.resolve(__dirname, './src/layouts'),
+        '@scripts': path.resolve(__dirname, './src/scripts'),
+        '@styles': path.resolve(__dirname, './src/styles'),
+      },
+    },
   },
 });
