7f2fbd6391989b47d2f15ad78f8b5107f05a41bf
rwbrockhoff <brockhoffrw@gmail.com>
2025-05-28 16:40:53 -0600

    feat: blog image component

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import mdx from '@astrojs/mdx';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [mdx()],
   vite: {
     resolve: {
       // alias path configuration
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,7 @@
 import { z, defineCollection } from 'astro:content';
 
 const blogCollection = defineCollection({
+  type: 'content',
   schema: z.object({
     title: z.string(),
     description: z.string(),
