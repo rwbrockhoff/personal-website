769099c7c4f8371bc55eac07bedfcd70af67f77d
rwbrockhoff <brockhoffrw@gmail.com>
2025-05-29 12:31:42 -0600

    feat: u-foods initial blog post, minor style changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,7 @@ export default defineConfig({
     resolve: {
       // alias path configuration
       alias: {
+        '@assets': path.resolve(__dirname, './src/assets'),
         '@components': path.resolve(__dirname, './src/components'),
         '@data': path.resolve(__dirname, './src/data'),
         '@images': path.resolve(__dirname, './src/assets/images'),
